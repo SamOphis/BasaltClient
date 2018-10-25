6107bf23cf9edd039b23a570d9ffb4f0c05d743f
Sam Pritchard <ophisbonklol@gmail.com>
2018-10-26 00:17:37 +0100

    remove keys from most requests (basalt v4) + renames

--- src/main/kotlin/com/github/samophis/basalt/client/entities/messages/client/PlayRequest.kt
+++ src/main/kotlin/com/github/samophis/basalt/client/entities/messages/client/PlayRequest.kt
@@ -17,6 +17,6 @@
 package com.github.samophis.basalt.client.entities.messages.client
 
 @Suppress("UNUSED")
-class PlayRequest internal constructor(val key: String, val guildId: String, val track: String, val startTime: Long? = null) {
+class PlayRequest internal constructor(val guildId: String, val track: String, val startTime: Long? = null) {
     val op = "play"
 }
